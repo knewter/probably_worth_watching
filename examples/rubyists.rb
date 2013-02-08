d9a6a2b39ea698e4a2fa1837a325b05c5dcc3375
Josh Adams <josh@isotope11.com>
2013-02-08 09:39:07 -0600

    Add rubyists back

--- examples/rubyists.rb
+++ examples/rubyists.rb
@@ -4,8 +4,62 @@ class Rubyists
 
   def twitter_handles
     %w(
+      JEG2
+      avdi
+      bascule
       knewter
       adamgamble
+      chadfowler
+      defunkt
+      pragdave
+      david_a_black
+      drnic
+      dhh
+      drbrain
+      evanphx
+      ezmobius
+      AkitaOnRails
+      topfunky
+      gilesgoatboy
+      jimweirich
+      peterc
+      wycats
+      _solnic_
+      the_zenspider
+      mfeathers
+      konstantinhaase
+      r00k
+      dkubb
+      eliseworthy
+      brynary
+      geeksam
+      kytrinyx
+      mattwynne
+      kevinrutherford
+      sarahmei
+      mpapis
+      sandimetz
+      steveklabnik
+      jstorimer
+      russolsen
+      PragmaticAndy
+      merbist
+      edavis10
+      KentBeck
+      garybernhardt
+      noelrap
+      joshsusser
+      j3
+      ryandotsmith
+      brixen
+      ReinH
+      mattyoho
+      indirect
+      mislav
+      bscofield
+      greggpollack
+      dbrady
+      MilesForrest
     )
   end
 end
