338f6b39e7726c4ad38e90549b0e47f053f6a316
Josh Adams <josh@isotope11.com>
2013-02-14 17:28:47 -0600

    Support jsonp

--- lib/probably_worth_watching/video.rb
+++ lib/probably_worth_watching/video.rb
@@ -11,5 +11,15 @@ module ProbablyWorthWatching
     property :duration, Object
     property :url, Object
     property :embed, Text
+
+    def to_json(*a)
+      {
+        title: title,
+        description: description,
+        duration: duration.number,
+        url: url,
+        embed: embed
+      }
+    end
   end
 end
--- site/app.rb
+++ site/app.rb
@@ -1,16 +1,20 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'slim'
-require 'pry'
+require 'json'
 require_relative '../lib/probably_worth_watching'
 
 get '/' do
-  @videos = ProbablyWorthWatching::Video.all
+  @videos = ProbablyWorthWatching::Video.all.reverse
   slim :index
 end
 
 get '/videos.json' do
-  @videos = ProbablyWorthWatching::Video.all
+  @videos = ProbablyWorthWatching::Video.all.reverse
   content_type :json
-  @videos.to_json
+  if callback = params[:callback]
+    "#{callback}(#{@videos.to_json})"
+  else
+    @videos.to_json
+  end
 end
